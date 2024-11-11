8cc60f9fa48d2669f1640a06293183f61243086d
Shanmukha Sai Guntupalli <147482584+Shanmuk890@users.noreply.github.com>
2024-11-11 17:06:18 +0530

    Update code.R

--- code.R
+++ code.R
@@ -1,34 +1,11 @@
 # Load the plumber package
 library(plumber)
 
-# Create the function that sums two numbers
-# This function will be exposed as an API endpoint
-# The numbers will be passed as query parameters
-
-#* @get /sum
-#* @param a First number
-#* @param b Second number
-#* @response 200 Returns the sum of two numbers
-function(a, b) {
-  # Convert input to numeric and sum them
-  a <- as.numeric(a)
-  b <- as.numeric(b)
-  
-  if (is.na(a) | is.na(b)) {
-    return(list(error = "Both a and b must be valid numbers"))
-  }
-  
-  # Sum the numbers
-  result <- a + b
-  return(list(sum = result))
+#' Add two numbers
+#' @param x A number
+#' @param y A number
+#' @get /add
+function(x = 0, y = 0) {
+  result <- as.numeric(x) + as.numeric(y)
+  return(list(result = result))
 }
-
-#* Swagger UI
-#* @get /swagger
-function() {
-  pr$swagger()  # This will serve the Swagger UI at /swagger endpoint
-}
-
-pr$run(host = '0.0.0.0', port = 8000) 
-
-
