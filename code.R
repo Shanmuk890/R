e1a1d59f9fb2755abf1fa4aef886c25b0c6adc0c
Shanmukha Sai Guntupalli <147482584+Shanmuk890@users.noreply.github.com>
2024-11-11 16:44:18 +0530

    Update code.R

--- code.R
+++ code.R
@@ -29,8 +29,4 @@ function() {
   pr$swagger()  # This will serve the Swagger UI at /swagger endpoint
 }
 
-# Create the plumber router
-pr <- plumb('/usr/local/src/app/code.R')
 
-# Run the Plumber API on port 8000
-pr$run(host = '0.0.0.0', port = 8000)
